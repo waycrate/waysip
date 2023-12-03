3620ac79f06db9d91a6a7d57102ff8fdb8e44ce2
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-12-03 21:54:02 +0800

    chore: base waysip

--- waysip/src/main.rs
+++ waysip/src/main.rs
@@ -1,4 +1,44 @@
+use clap::Parser;
 use libwaysip::get_area;
+
+#[derive(Debug, Parser)]
+#[command(name = "waysip")]
+#[command(about="a tool like slurp, but in rust", long_about = None)]
+enum Cli {
+    #[command(short_flag = 'p')]
+    Point,
+    #[command(short_flag = 'd')]
+    Dimesions,
+}
+
 fn main() {
-    println!("{:?}", get_area());
+    let args = Cli::parse();
+
+    let info = match get_area() {
+        Ok(Some(info)) => info,
+        Ok(None) => {
+            eprintln!("Get None, you cancel it");
+            return;
+        }
+        Err(e) => {
+            eprintln!("Error,{e}");
+            return;
+        }
+    };
+
+    match args {
+        Cli::Point => {
+            let (x, y) = info.left_top_point();
+            println!("{},{} 1x1", x as i32, y as i32);
+        }
+        Cli::Dimesions => {
+            let (x, y) = info.left_top_point();
+            let width = info.width();
+            let height = info.height();
+            println!(
+                "{},{} {}x{}",
+                x as i32, y as i32, width as i32, height as i32
+            );
+        }
+    }
 }
