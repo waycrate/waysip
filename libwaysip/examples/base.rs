ec3eb5c0f79f6ad3e667a7ab1e9d8bf43984e782
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2025-08-12 09:57:47 +0900

    chore: update deps

--- libwaysip/examples/base.rs
+++ libwaysip/examples/base.rs
@@ -1,21 +1,5 @@
 use libwaysip::{Color, SelectionType, WaySip};
-use wayland_client::{
-    Connection, Dispatch, QueueHandle, globals::GlobalListContents, protocol::wl_registry,
-};
-
-struct State {}
-
-impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for State {
-    fn event(
-        _: &mut State,
-        _: &wl_registry::WlRegistry,
-        _: wl_registry::Event,
-        _: &GlobalListContents,
-        _: &Connection,
-        _: &QueueHandle<State>,
-    ) {
-    }
-}
+use wayland_client::Connection;
 
 fn main() {
     let connection = Connection::connect_to_env().unwrap();
